0cb23eb6cfc1706f84b85f64932628d3f61fde2a
dmedinas <dmedinas@gmail.com>
2023-12-28 17:21:53 +0100

    Optimizados estilos de la página principal

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const mainBanner = document.getElementById('top');
 const divApp = document.getElementById('app');
 const home = document.querySelector('.home')
 const planNav = document.querySelector('.planNav')
+const logo = document.querySelector('.logo')
+const logoFoot = document.querySelector('.logoFoot')
 
 planButton.addEventListener('click', (event) =>{
     divApp.classList.remove('hiddenApp');
@@ -17,6 +19,18 @@ planNav.addEventListener('click', (event) =>{
 })
 
 home.addEventListener('click', (event) =>{
+    divApp.classList.add('hiddenApp');
+    mainBanner.classList.remove('hiddenBanner');
+    mainBanner.classList.add('shadowBanner');
+})
+
+logo.addEventListener('click', (event) =>{
+    divApp.classList.add('hiddenApp');
+    mainBanner.classList.remove('hiddenBanner');
+    mainBanner.classList.add('shadowBanner');
+})
+
+logoFoot.addEventListener('click', (event) =>{
     divApp.classList.add('hiddenApp');
     mainBanner.classList.remove('hiddenBanner');
     mainBanner.classList.add('shadowBanner');
